e7991f4772cddbdf1f48175a4019b1e66fbbb476
Thomas Moore <tom.moore292@gmail.com>
2018-03-21 15:00:19 +0000

    content

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -26,8 +26,26 @@ function collegeBlog_search_form( $form ) {
 	return $form;
 }
 add_filter( 'get_search_form', 'collegeBlog_search_form', 100 );
+
 // DISABLE WORDPRESS COMMENTS JS
 function itsg_disable_comment_js(){
     wp_deregister_script( 'comment-reply' );
 }
 add_action( 'init', 'itsg_disable_comment_js' );
+
+// Edit length of excerpt
+function wpdocs_custom_excerpt_length( $length ) {
+    return 20;
+}
+add_filter( 'excerpt_length', 'wpdocs_custom_excerpt_length', 999 );
+
+/**
+ * Filter the excerpt "read more" string.
+ *
+ * @param string $more "Read more" excerpt string.
+ * @return string (Maybe) modified "read more" excerpt string.
+ */
+function wpdocs_excerpt_more( $more ) {
+  return '...';
+}
+add_filter( 'excerpt_more', 'wpdocs_excerpt_more' );
