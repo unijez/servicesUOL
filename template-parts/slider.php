448df47d424a000031291046615491b29e8eec9a
Brennan Ceballos Pena <bpena@lincoln.ac.uk>
2018-04-11 15:55:22 +0100

    Added: Slider into template parts and made includable through template part

---
+++ template-parts/slider.php
@@ -0,0 +1,57 @@
+<div class="main-slider">
+  <?php
+  $args = array(
+    'post_type' => 'post',
+    'posts_per_page' => 3
+//			'meta_query' => array(
+//											array(
+//			'compare' => 'EXISTS'
+//			),)
+  );
+
+  $recent_posts = new WP_Query( $args );
+
+  if( $recent_posts->have_posts() ): ?>
+
+    <div class="hero-header">
+
+      <?php while ($recent_posts->have_posts() ) : $recent_posts->the_post() ?>
+
+      <div class="slick-slide heading-image" style="background-image: url(<?php echo header_post_image(); ?>)">
+        <?php get_image_result(header_post_image()); ?>
+        <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title() ?></a>
+
+        <?php
+        if(has_post_thumbnail()):
+
+          $imgdata = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'hero-header', false);
+          $imgwidth = $imgdata[1];
+          $wanted_width = 1600;
+        endif
+        ?>
+        <div class="overlay-slide item">
+          <div class="item-text">
+            <h5 class="header-text-area">
+              <?php echo get_the_date(); ?>
+            </h5>
+            <a href="<?php the_permalink() ?>">
+              <h2 class="header-text-area"><?php the_title(); ?></h2>
+            </a>
+            <div>
+                <a href="<?php the_permalink()?>" class="header-text-area">Read More</a>
+            </div> <!-- author -->
+          </div>
+        </div>
+
+      </div>
+      <?php
+      endwhile;
+      wp_reset_query();
+      ?>
+    </div>
+  <?php else : ?>
+
+  <h2>Ooops, no posts here!</h2>
+
+  <?php endif; wp_reset_query(); ?>
+</div>
