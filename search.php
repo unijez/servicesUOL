c46be1fe3021615ca7ff119f02d58fabc73675ae
Brennan Ceballos Pena <bpena@lincoln.ac.uk>
2018-04-18 10:22:03 +0100

    Added: Styling for the search bar and the search page, as well as changing a few things on the page

--- search.php
+++ search.php
@@ -14,38 +14,36 @@
 get_header(); ?>
 
 	<main id="main" class="site-main">
-			
-			
-			
+
 			<?php if ( have_posts() ) : ?>
-			
-					
+
+					<div class="content-container searched-cont">
 					<h1 class="page-title">
 						<?php
 						/* translators: %s: search query. */
-						printf( esc_html__( 'Search Results for: %s', 'ServicesUOL' ), '<span>' . get_search_query() . '</span>' );
+						printf( esc_html__( 'Search Results for: %s', 'ServicesUOL' ), '<span>' . get_search_query() . '</span>&emsp;<i class="far fa-angle-down"></i>' );
 						?>
 					</h1>
-					
-			
-			
+
+
+
 			 		<?php while ( have_posts() ) : the_post(); ?>
-			
-										
+
+
 								<?php get_template_part( 'template-parts/content', 'search' ); ?>
-									
-			
+
+
 			 		<?php endwhile; ?>
-			 						
+
 			 						<?php the_posts_navigation(); ?>
-			 					
+
 			 		<?php else:  ?>
-			 					
-			 					
+
+
 			 		<?php get_template_part( 'template-parts/content', 'none' ); ?>
-			 					
-			 					
-			
+
+					</div>
+
 			<?php endif;  wp_reset_query(); ?>
 
 
--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -14,21 +14,21 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header clear">
+	<header class="entry-header clear searched-header">
 		<?php the_title( sprintf( '<h2 class="page-title__inner"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
 		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
+		<div class="entry-meta searched-date">
 			<?php ServicesUOL_posted_on(); ?>
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<div class="entry-summary">
+	<div class="entry-summary searched-summary">
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-summary -->
 
-	<footer class="entry-footer">
+	<footer class="entry-footer searched-footer">
 		<?php ServicesUOL_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
