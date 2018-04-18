7077e26307c5e5b9be0f430fdc96731987a8725a
Brennan Ceballos Pena <bpena@lincoln.ac.uk>
2018-04-18 11:43:44 +0100

    Added: Styling for post pages

--- template-parts/featured-image.php
+++ template-parts/featured-image.php
@@ -24,7 +24,7 @@
 
   <div class="title-container">
     <i class="calendar-icon fal fa-calendar-alt"></i><time class="news-post-date date-published" datetime="<?php the_time('d/m/Y') ?>"><?php the_time('jS F, Y') ?></time>
-    <?php the_title( '<h1 class="page-title__inner">', '</h1>' ); ?>
+    <?php the_title( '<h1 class="page-title__inner posted">', '</h1>' ); ?>
   </div>
 
 <?php endif; ?>
