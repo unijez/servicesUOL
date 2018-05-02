02ac72f7e7a1b7b4dce09424cef98e37617877eb
Thomas Moore <tom.moore292@gmail.com>
2018-05-02 12:21:19 +0100

    Mainly MQ work through out the site. Generally tidying up content styles here and there

--- single.php
+++ single.php
@@ -27,19 +27,59 @@
 
 								<?php get_template_part( 'template-parts/content', get_post_type() ); ?>
 
+                <?php
+                //Is there a next or previous post?
+                $prev_link = get_previous_post_link();
+                $next_link = get_next_post_link();
 
-				                <?php if ( comments_open() || get_comments_number() ) : ?>
+                //Get the next or previous link url
+                $previous = get_previous_post();
+                $next = get_next_post();
+
+                ?>
+
+                <?php if ($prev_link || $next_link): ?>
+
+	                 <ul class="single-pagination">
+
+		                     <li class="single-pagination__link">
+
+			                      <?php if ($prev_link) : ?>
+
+						                   <a class="main-btn block-button dark" href="<?php echo $previous->guid ?>"><i class="far fa-angle-left"></i>  Previous Post</a>
+
+			                      <?php endif; ?>
+
+		                     </li>
+
+		                     <li class="single-pagination__link">
+
+		                         <?php if ($next_link) : ?>
+
+					                     <a class="main-btn block-button dark" href="<?php echo $next->guid ?>">Next Post <i class="far fa-angle-right"></i></a>
+
+		                         <?php endif; ?>
+		                    </li>
+
+ 	                 </ul><!-- single-pagination -->
 
 
-				                  	<div class="comments-container">
+                 <?php endif; ?>
 
-					                  <?php  comments_template(); ?>
 
-				                	</div> <!-- comments-container -->
+
+				         <?php if ( comments_open() || get_comments_number() ) : ?>
+
+
+				           <div class="comments-container">
+
+					              <?php  comments_template(); ?>
+
+				           </div> <!-- comments-container -->
 
           </div>
 
-				              <?php endif; ?>
+				         <?php endif; ?>
 
 
  					<?php endwhile; ?>
--- template-parts/content.php
+++ template-parts/content.php
@@ -25,7 +25,4 @@
 		?>
 	</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-		<?php ServicesUOL_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
