b0d719da71517ac368eaabb85669db6678fe28ab
Brennan Ceballos Pena <bpena@lincoln.ac.uk>
2018-05-22 11:59:45 +0100

    Added: Padding / Content container to the 404 page

--- 404.php
+++ 404.php
@@ -14,14 +14,14 @@
 get_header(); ?>
 
 	<main id="main" class="site-main">
-				
-				<div class="container-404">
-					
+
+				<div class="content-container">
+
 							<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'ServicesUOL' ); ?></h1>
-						
+
 							<p><?php esc_html_e( 'It looks like nothing was found at this location.', 'ServicesUOL' ); ?></p>
 
-						
+
 				</div> <!-- 404-container -->
 
 	</main><!-- #main -->
