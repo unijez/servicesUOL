bcc8f037a2e164a2d208ffc7809a12299421f1da
Brennan Ceballos Pena <bpena@lincoln.ac.uk>
2018-06-04 10:56:08 +0100

    Added: updated functionality to the acfs

--- inc/acfs/maximum-title-length-acf.php
+++ inc/acfs/maximum-title-length-acf.php
@@ -17,7 +17,7 @@ acf_add_local_field_group(array(
 				'class' => '',
 				'id' => '',
 			),
-			'default_value' => '',
+			'default_value' => 200,
 			'placeholder' => '',
 			'prepend' => '',
 			'append' => '',
